0a057c423a3a9efe132386ed4a82483592b45090
MarinaNovaes0 <marinanovaes0800@gmail.com>
2024-09-26 08:56:47 -0300

    atividade01 aula03

---
+++ aula03/atividade.py
@@ -0,0 +1,13 @@
+nome = input('Digite o seu nome: ')
+nasc = input('Digite o ano que você nasceu: ')
+cpf = input('Digite o seu cpf: ')
+
+print('Seu nome é:',nome) 
+print('Você nasceu em:',nasc) 
+print('Seu cpf é:',cpf)
+nasc = int(nasc)
+cpf = int(cpf)
+
+print(type(nome))
+print(type(nasc))
+print(type(cpf))
